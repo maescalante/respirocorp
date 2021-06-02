9ecf9640887040e3c28de90ff0f779b2e380277f
maescalante <ma.escalante@uniandes.edu.co>
2021-06-02 16:47:28 -0500

    under construction

--- src/App.js
+++ src/App.js
@@ -1,25 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import React from 'react'
+import construction from "./assets/img/construction.png"
+import "./css/App.css"
+const App = () => {
+    return (
+        <div >  
+            <h1>RespirO<sub>2</sub>Corp</h1>
+            <h3>Sitio Web en Construcción </h3>
+            <img src={construction} alt="sitio en construcción"/>
+        </div>
+    )
 }
 
-export default App;
+export default App
