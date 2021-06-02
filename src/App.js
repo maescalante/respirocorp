14af3b7bc12e7ed12666628cfa62366f500569ae
maescalante <ma.escalante@uniandes.edu.co>
2021-06-02 17:31:44 -0500

    cambios

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React from 'react'
 import construction from "./assets/img/construction.png"
+import {Row, Col} from "react-bootstrap"
 import "./css/App.css"
 const App = () => {
     return (
         <div >  
-            <h1>RespirO<sub>2</sub>Corp</h1>
+            <h1>Corporación RespirO<sub>2</sub> de Colombia</h1>
             <h3>Sitio Web en Construcción </h3>
             <img src={construction} alt="sitio en construcción"/>
         </div>
