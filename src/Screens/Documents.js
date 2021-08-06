998e8df58341b3fa4a90c8f02b6dd2483c6ca384
maescalante <ma.escalante@uniandes.edu.co>
2021-08-06 19:32:58 +0200

    docs

--- src/Screens/Documents.js
+++ src/Screens/Documents.js
@@ -1,5 +1,11 @@
 import React from "react";
 import { Container, Button } from "react-bootstrap";
+import File1 from "../assets/docs/151_BALANCE_INICIAL_FIRMADO.pdf";
+import File2 from "../assets/docs/151_CERTIFICADO_ANTECEDENTES_DIRECTIVOS.pdf";
+import File3 from "../assets/docs/151_CERTIFICADO_CUMPLIMIENTO_364_3.pdf";
+import File4 from "../assets/docs/151_ESTATUTOS_ANEXO_1.pdf";
+import File5 from "../assets/docs/151_REMUNERACION_DIRECTIVOS.pdf";
+import File6 from "../assets/docs/151_RUT_ACTUALIZADO.pdf";
 
 const Documents = () => {
   return (
@@ -15,9 +21,29 @@ const Documents = () => {
           </p>
         </div>
         <div className="row">
-          <Button className="downloads" href="path_to_file" download="RTE">
+          <Button className="downloads" href={File1} download>
             {" "}
-            Estados financieros y revelaciones
+            151_BALANCE_INICIAL_FIRMADO
+          </Button>
+          <Button className="downloads" href={File2} download>
+            {" "}
+            151_CERTIFICADO_ANTECEDENTES_DIRECTIVOS
+          </Button>
+          <Button className="downloads" href={File3} download>
+            {" "}
+            151_CERTIFICADO_CUMPLIMIENTO_364_3
+          </Button>
+          <Button className="downloads" href={File4} download>
+            {" "}
+            151_ESTATUTOS_ANEXO_1
+          </Button>
+          <Button className="downloads" href={File5} download>
+            {" "}
+            151_REMUNERACION_DIRECTIVOS
+          </Button>
+          <Button className="downloads" href={File6} download>
+            {" "}
+            151_RUT_ACTUALIZADO
           </Button>
         </div>
       </Container>
